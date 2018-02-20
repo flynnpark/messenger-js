12abeffdb95c5a2b4fcb9b4566e187688e3eaf5a
Flynn <wphestiraid@gmail.com>
2018-02-20 23:06:45 +0900

    초기 앱 설정
    - morgan, mongoose 사용
    - mongoDB 연결

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -13,7 +16,14 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// set the secret key variable for jwt
+app.set('jwt-secret', config.secret);
 
+// parse JSON and url-urlencoded query
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// print the request log on console
+app.use(morgan('dev'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -41,6 +51,13 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+});
+
+mongoose.connect(config.mongodbUri);
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log('Connected to mongodb server');
 });
 
 module.exports = app;
